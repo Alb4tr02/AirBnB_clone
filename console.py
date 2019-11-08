98208b51e6958bbef41efa8394184197c4121986
diegolopezq95 <927@holbertonschool.com>
2019-11-08 19:38:25 +0000

    [Feature]: Command interpreter (quit, EOF and help

---
+++ console.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" This module create class HBNBCommand.
+contains the entry point of the command interpreter
+"""
+
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    class HBNBCommand
+    """
+    prompt = "(hbnb) "
+
+
+    def do_quit(self, argument):
+        """
+        Quit command to exit the program
+        """
+        return True
+
+    def do_EOF(self, argument):
+        """
+        EOF (Ctr^D) to exit the program
+        """
+        print()
+        return True
+
+    def emptyline(self):
+        """
+        An empty line that execute anything
+        """
+        pass
+
+if __name__ == '__main__':
+    """
+    Code not be executed when imported
+    """
+    HBNBCommand().cmdloop()
