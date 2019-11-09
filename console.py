9f1a98ecc0b1db78d11372bc09f96d3965efd3ba
diegolopezq95 <927@holbertonschool.com>
2019-11-09 17:00:38 +0000

    [Update]: adding show to command interpreter

--- console.py
+++ console.py
@@ -6,7 +6,7 @@ contains the entry point of the command interpreter
 
 import cmd
 from models.base_model import BaseModel
-
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
     """
@@ -52,6 +52,25 @@ class HBNBCommand(cmd.Cmd):
             new_instance = eval(argv[0] + "()")
             new_instance.save()
             print(new_instance.id)
+
+    def do_show(self, arg):
+        """
+        Prints the string representation of an instance
+        based on the class name and id
+        """
+        argv = arg.split()
+        if len(argv) == 0:
+            print("** class name missing **")
+        elif argv[0] not in self.class_list:
+            print("** class doesn't exist **")
+        elif argv[0] in self.class_list and len(argv) == 1:
+            print("** instance id missing **")
+        else:
+            obj_attr = str(argv[0] + "." + argv[1])
+            if obj_attr in storage.all():
+                print(storage.all()[obj_attr])
+            else:
+                print("** no instance found **")
 
 if __name__ == '__main__':
     """
