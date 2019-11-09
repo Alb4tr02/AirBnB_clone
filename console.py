b5c8f3a189f960d7866dae0f3a44874bff08ab3c
diegolopezq95 <927@holbertonschool.com>
2019-11-09 20:50:45 +0000

    [Update]: Fix all and create update to command interpreter

--- console.py
+++ console.py
@@ -5,6 +5,7 @@ contains the entry point of the command interpreter
 
 
 import cmd
+import shlex
 from models.base_model import BaseModel
 from models import storage
 
@@ -98,7 +99,7 @@ class HBNBCommand(cmd.Cmd):
         based or not on the class name
         """
         argv = arg.split()
-        if argv[0] not in self.class_list:
+        if len(argv) >= 1 and argv[0] not in self.class_list:
             print("** class doesn't exist **")
         else:
             obj_list = []
@@ -106,6 +107,31 @@ class HBNBCommand(cmd.Cmd):
                 key = storage.all()[obj_attr]
                 obj_list.append(str(key))
             print(obj_list)
+
+    def do_update(self, arg):
+        """
+        Updates an instance based on the class name and id
+        by adding or updating attribute
+        """
+        argv = shlex.split(arg)
+        if len(argv) == 0:
+            print("** class name missing **")
+        elif argv[0] not in self.class_list:
+            print("** class doesn't exist **")
+        elif argv[0] in self.class_list and len(argv) == 1:
+            print("** instance id missing **")
+        elif len(argv) < 3:
+            print("** attribute name missing **")
+        elif len(argv) < 4:
+            print("** value missing **")
+        else:
+            obj_attr = str(argv[0] + "." + argv[1])
+            if obj_attr not in storage.all():
+                print("** no instance found **")
+            else:
+                setattr(storage.all()[obj_attr], argv[2], argv[3])
+                storage_update = storage.all()
+                storage_update[obj_attr].save()
 
 if __name__ == '__main__':
     """
