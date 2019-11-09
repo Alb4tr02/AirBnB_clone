b79a243027af22e8b2feb71df157d6f34df1f419
diegolopezq95 <927@holbertonschool.com>
2019-11-09 15:55:44 +0000

    [Update]: adding create to command interpreter

--- console.py
+++ console.py
@@ -5,6 +5,7 @@ contains the entry point of the command interpreter
 
 
 import cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -12,14 +13,20 @@ class HBNBCommand(cmd.Cmd):
     class HBNBCommand
     """
     prompt = "(hbnb) "
+    class_list = ["BaseModel",
+                  "State",
+                  "City",
+                  "Amenity",
+                  "Place",
+                  "Review"]
 
-    def do_quit(self, argument):
+    def do_quit(self, arg):
         """
         Quit command to exit the program
         """
         return True
 
-    def do_EOF(self, argument):
+    def do_EOF(self, arg):
         """
         EOF (Ctr^D) to exit the program
         """
@@ -31,6 +38,20 @@ class HBNBCommand(cmd.Cmd):
         An empty line that execute anything
         """
         pass
+
+    def do_create(self, arg):
+        """
+        Creates a new instance of BaseModel
+        """
+        argv = arg.split()
+        if len(argv) == 0:
+            print("** class name missing **")
+        elif argv[0] not in self.class_list:
+            print("** class doesn't exist **")
+        else:
+            new_instance = eval(argv[0] + "()")
+            new_instance.save()
+            print(new_instance.id)
 
 if __name__ == '__main__':
     """
